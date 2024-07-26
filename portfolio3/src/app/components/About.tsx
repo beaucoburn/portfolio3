2c54867f501ad2596712a90812c3e6d07f0ca522
beaucoburn <douglas.coburn@gmail.com>
2024-07-25 19:17:12 -0600

    Add About section, FullScreen, played with CSS

--- portfolio3/src/app/components/About.tsx
+++ portfolio3/src/app/components/About.tsx
@@ -1,7 +1,24 @@
+import FullScreenSection from "./FullScreenSection";
+import { Heading, VStack, HStack, Text } from "@chakra-ui/react";
+
+
 export default function About(){
   return (
-    <div>
-      About
-    </div>
+    <FullScreenSection
+      backgroundColor="#ccc8cd"
+      py={16}
+      spacing={8}
+     // isDarkBackground
+      id="about-section"
+      backgroundAttachment="fixed"
+      boxShadow="inset 0 0 5em rgba(0, 0, 0, 0.5)"
+    >
+      <VStack py={12} position="relative">
+        <Heading py={8}>About Me</Heading>
+        <Text py={8} px={60} size="xl">
+          As a passionate Frontend Developer from the United States, my expertise lies in crafting seamless and visually appealing web experiences. My primary focus revolves around utilizing cutting-edge technologies, with a strong proficiency in React and NextJS to build dynamic and responsive user interfaces. Leveraging the power of Tailwind CSS, I bring a keen eye for design and a commitment to creating intuitive and efficient user interfaces. With a deep understanding of web development principles and a penchant for staying abreast of the latest industry trends, I am dedicated to delivering high-quality, performance-driven applications that resonate with both clients and end-users. My journey as a Frontend Developer is characterized by a commitment to innovation, attention to detail, and a genuine passion for creating exceptional digital experiences.
+        </Text>
+      </VStack>
+    </FullScreenSection>
   )
 }
--- portfolio3/src/app/components/Hero.tsx
+++ portfolio3/src/app/components/Hero.tsx
@@ -1,7 +1,10 @@
+
+
 import { Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 import Image from "next/image";
 import Headshot from ".//../images/headshot.jpg";
+import Background from ".//../images/maik-jonietz-_yMciiStJyY-unsplash.jpg";
 
 const greeting = "Hi, I'm Beau Coburn!";
 const bio1 = "I'm a software developer and I love building things.";
@@ -12,20 +15,28 @@ export default function Hero() {
     <FullScreenSection
       justifyContent="center"
       alignItems="center"
-      isDarkBackground
-      backgroundColor="#2c698d"
+    //  isDarkBackground
+    //  backgroundImage={`url(${".//../images/maik-jonietz-_yMciiStJyY-unsplash.jpg"})`}
+    //  backgroundSize="cover"
+    //  backgroundPosition="center"
+      backgroundColor="#303036"
+      zIndex={10}
     >
       <VStack>
+      <div style={{ borderRadius: "40px", overflow: "hidden", border: "20px solid #303036", boxShadow: "  20px 20px 60px #989898, -20px -20px 60px #000000", transform: "rotateY(30deg) translateZ(10px)", transformStyle: "preserve-3d"}}>
         <Image
           src={Headshot}
           alt="Beau"
           width={225}
-          height={200}
- //         borderRadius={50}
+          height="auto"
+          priority
         />
+        </div>
+        <div style={{ backgroundColor: "#e0e0e0", padding: "10px", borderRadius: "40px", overflow: "hidden", /*border: "20px solid #e0e0e0",*/ boxShadow: "  20px 20px 60px #989898, -20px -20px 60px #000000", transform: "translateX(-1px) scale(1.01)"}}>
         <Heading size="2xl" my={8}>{greeting}</Heading>
         <Heading size="xl" my={8}>{bio1}</Heading>
         <Heading size="xl" my={8}>{bio2}</Heading>
+        </div>
       </VStack>
     </FullScreenSection>
   );
