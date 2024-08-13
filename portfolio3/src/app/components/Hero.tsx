cc2de50dfea8cf96870f9dc1c99c2929c93c0516
beaucoburn <douglas.coburn@gmail.com>
2024-08-13 17:41:51 -0600

    modified:   portfolio3/src/app/components/Hero.tsx

--- portfolio3/src/app/components/Hero.tsx
+++ portfolio3/src/app/components/Hero.tsx
@@ -48,24 +48,20 @@ import Image from "next/image";
 import React from "react";
 import { CardBody, CardContainer, CardItem } from "../../components/ui/3d-card";
 import Headshot from ".//../images/headshot.jpg";
+import FullScreenSection from "./FullScreenSection";
+
+const greeting = "Hi, I'm Beau Coburn!";
+const bio1 = "I'm a software developer and I love building things.";
+const bio2 = "I specialize in React and NextJS.";
 
 export default function Hero() {
   return (
+    <FullScreenSection
+      isDarkBackground
+      backgroundColor="#303036"
+      >
     <CardContainer className="inter-var">
-      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
-        <CardItem
-          translateZ="50"
-          className="text-xl font-bold text-neutral-600 dark:text-white"
-        >
-          Make things float in air
-        </CardItem>
-        <CardItem
-          as="p"
-          translateZ="60"
-          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-        >
-          Hover over this card to unleash the power of CSS perspective
-        </CardItem>
+      <CardBody className="bg-gray-60 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
         <CardItem
           translateZ="100"
           rotateX={20}
@@ -74,32 +70,54 @@ export default function Hero() {
         >
           <Image
             src={Headshot}
-            height="1000"
+            height="3000"
             width="1000"
-            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+            border="1px solid #000000"
+            className="h-100 w-full object-cover rounded-xl group-hover/card:shadow-xl"
             alt="thumbnail"
           />
         </CardItem>
-        <div className="flex justify-between items-center mt-20">
-          <CardItem
-            translateZ={20}
-            translateX={-40}
-            as="button"
-            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
-          >
-            Try now →
-          </CardItem>
-          <CardItem
-            translateZ={20}
-            translateX={40}
-            as="button"
-            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
-          >
-            Sign up
-          </CardItem>
-        </div>
+        <CardItem
+          translateZ="50"
+          translateX={40}
+          className="text-xl font-bold text-neutral-600 dark:text-white"
+        >
+        <CardItem
+          as="h1"
+          className="text-xl font-bold text-white pt-4 dark:text-white"
+        >
+        {greeting} 
+        </CardItem>
+        </CardItem>
+
+        <CardItem
+          translateZ="50"
+          translateX={-20}
+          className="text-xl font-bold text-neutral-600 dark:text-white"
+        >
+        <CardItem
+          as="h1"
+          className="text-xl font-bold text-white pt-4 dark:text-white"
+        >
+          {bio1}
+        </CardItem>
+        </CardItem>
+
+        <CardItem
+          translateZ="50"
+          translateX={20}
+          className="text-xl font-bold text-neutral-600 dark:text-white"
+        >
+        <CardItem
+          as="h1"
+          className="text-xl font-bold text-white pt-4 dark:text-white"
+        >
+        {bio2} 
+        </CardItem>
+        </CardItem>
       </CardBody>
     </CardContainer>
+    </FullScreenSection>
   );
 }
 
