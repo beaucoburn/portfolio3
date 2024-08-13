9ad03bff48a6cef97038022a34639fd46d8b7e10
beaucoburn <douglas.coburn@gmail.com>
2024-08-13 14:06:35 -0600

    modified:   portfolio3/package-lock.json 	modified:   portfolio3/package.json 	modified:   portfolio3/src/app/components/Hero.tsx 	modified:   portfolio3/src/app/page.tsx

--- portfolio3/src/app/page.tsx
+++ portfolio3/src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+//import Image from "next/image";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import About from "./components/About";
