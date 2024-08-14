e4ecdae55d0b4613e1af5ebcbcf49a626779237d
beaucoburn <douglas.coburn@gmail.com>
2024-08-14 11:00:45 -0600

    Adjust text on Hero

--- portfolio3/src/app/components/Hero.tsx
+++ portfolio3/src/app/components/Hero.tsx
@@ -61,18 +61,18 @@ export default function Hero() {
       backgroundColor="#303036"
       >
     <CardContainer className="inter-var">
-      <CardBody className="bg-gray-60 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
         <CardItem
           translateZ="100"
           rotateX={20}
           rotateZ={-10}
           className="w-full mt-4"
+          border="1px solid #000000"
         >
           <Image
             src={Headshot}
             height="3000"
             width="1000"
-            border="1px solid #000000"
             className="h-100 w-full object-cover rounded-xl group-hover/card:shadow-xl"
             alt="thumbnail"
           />
@@ -84,33 +84,33 @@ export default function Hero() {
         >
         <CardItem
           as="h1"
-          className="text-xl font-bold text-white pt-4 dark:text-white"
+          className="text-xl font-bold text-black pt-4 dark:text-white"
         >
         {greeting} 
         </CardItem>
         </CardItem>
 
         <CardItem
-          translateZ="50"
-          translateX={-20}
+          translateZ="60"
+          translateX={-60}
           className="text-xl font-bold text-neutral-600 dark:text-white"
         >
         <CardItem
           as="h1"
-          className="text-xl font-bold text-white pt-4 dark:text-white"
+          className="text-xl font-bold text-black pt-4 dark:text-white"
         >
           {bio1}
         </CardItem>
         </CardItem>
 
         <CardItem
-          translateZ="50"
-          translateX={20}
+          translateZ="70"
+          translateX={60}
           className="text-xl font-bold text-neutral-600 dark:text-white"
         >
         <CardItem
           as="h1"
-          className="text-xl font-bold text-white pt-4 dark:text-white"
+          className="text-xl font-bold text-black pt-4 dark:text-white"
         >
         {bio2} 
         </CardItem>
