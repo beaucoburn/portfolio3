6e00adcec7999b56120b1bcbcf3f854eba2b8a54
beaucoburn <douglas.coburn@gmail.com>
2024-08-20 10:07:28 -0600

    modified:   portfolio3/src/app/components/Projects.tsx

--- portfolio3/src/app/components/Projects.tsx
+++ portfolio3/src/app/components/Projects.tsx
@@ -1,7 +1,55 @@
-export default function Projects(){
+import React from "react";
+import FullScreenSection from "./FullScreenSection";
+import { Box, Heading } from "@chakra-ui/react";
+import Card from "./Card";
+
+const projects = [
+  {
+    title: "April Elizabeth E-Commerce NextJS App",
+    description:
+      "An E-Commerce app built with NextJS, React, Tailwind CSS, and Shopify's GraphQL API.",
+    getImageSrc: () => require("../images/aprilart.png"),
+    url: "https://april-art-22sh.vercel.app/"
+  },
+  {
+    title: "Little Lemon Restaurant Reservation React App",
+    description:
+      "An addition to a restaurant reservation app built with React.",
+    getImageSrc: () => require("../images/restaurantChef.jpg"),
+    url: "https://little-lemon-rouge.vercel.app/"
+  },
+];
+
+const ProjectsSection = () => {
   return (
-    <div>
-      Projects
-    </div>
-  )
-}
+    <FullScreenSection
+      backgroundColor="#e3f6f5"
+      p={8}
+      alignItems="flex-start"
+      spacing={8}
+      id="projects-section"
+    >
+      <Heading as="h1" pt={16}>
+        Featured Projects
+      </Heading>
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(2,minmax(0,1fr))"
+        gridGap={8}
+        pt={32}
+      >
+        {projects.map((project) => (
+          <Card
+            key={project.title}
+            title={project.title}
+            description={project.description}
+            imageSrc={project.getImageSrc()}
+            url={project.url}
+          />
+        ))}
+      </Box>
+    </FullScreenSection>
+  );
+};
+
+export default ProjectsSection;
